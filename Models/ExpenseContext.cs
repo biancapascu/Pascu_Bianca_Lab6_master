2b0036d97eabeaee4ae38b0c19f578b0a5beba60
Bianca Pascu <biancapascu19@yahoo.com>
2022-11-17 18:18:36 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Pascu_Bianca_Lab6.Models
+{
+    public class ExpenseContext: DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options): base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+    }
+}
